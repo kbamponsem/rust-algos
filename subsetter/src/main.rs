3aff04799f945567ac3998d4ecbf89803095355e
kbamponsem <kwabenaamponsem@gmail.com>
2023-04-28 12:50:38 +0200

    Adds details of subsetter impl to subsetter

--- subsetter/src/main.rs
+++ subsetter/src/main.rs
@@ -1,3 +1,40 @@
+// Explain in details what is going on in this entire Rust program.
+
+// This is a macro that implements the SubSetter trait for all types that implement the
+// SubSetter trait. This is a macro because we want to implement the trait for all types
+// that implement the trait, and we can't do that with a normal impl block.
+
+// The macro is called impl_subsets, and it takes a list of types as arguments. The macro
+// expands to a bunch of impl blocks, one for each type. The macro is defined below the
+// main function.
+
+// The SubSetter trait is defined below the main function. It has one associated type, T,
+// and one method, subsets. The subsets method returns a vector of hash sets of T. The
+// subsets method is implemented for all types that implement the SubSetter trait.
+
+// The main function prints the subsets of the set {1, 2, 3, 4} and the subsets of the set
+// {"a", "b", "c"}. The subsets method is called on the sets, and the result is printed.
+
+// The assert_same function is defined below the main function. It takes two vectors of
+// hash sets of T, and asserts that they are the same. The assert_same function is called
+// in the tests module.
+
+// The set macro is defined below the assert_same function. It takes a list of elements as
+// arguments, and returns a hash set of those elements. The set macro is called in the
+// tests module.
+
+// The union macro is defined below the set macro. It takes two hash sets as arguments, and
+// returns the union of the two hash sets. The union macro is called in the tests module.
+
+// The tests module is defined below the union macro. It contains two tests. The first test
+// asserts that the set macro expands to the same thing as a manually constructed hash set.
+// The second test asserts that the subsets method returns the correct subsets for a few
+// different sets.
+
+// The macro is defined below the tests module. It takes a list of types as arguments. The
+// macro expands to a bunch of impl blocks, one for each type. The macro is called in the
+// main function.
+
 use std::{
     collections::HashSet,
     fmt::{Debug, Display},
